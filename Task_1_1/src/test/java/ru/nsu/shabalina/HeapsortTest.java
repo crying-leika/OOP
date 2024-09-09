f89091719eeb945562a2eb76c6e1ee120b3f8abc
leika <liliannashabalina124@gmail.com>
2024-09-09 23:27:17 +0700

    rename tests

--- Task_1_1/src/test/java/ru/nsu/shabalina/HeapsortTest.java
+++ Task_1_1/src/test/java/ru/nsu/shabalina/HeapsortTest.java
@@ -6,49 +6,49 @@ import org.junit.jupiter.api.Test;
 
 public class HeapsortTest {
     @Test
-    public void test1(){
+    public void testRandom(){
         int[] sut = {1, 3, 2, 2};
         int[] sortedSut = {1, 2, 2, 3};
         Assertions.assertArrayEquals(sortedSut, Heapsort.heapsort(sut));
     }
 
     @Test
-    public void test2(){
+    public void testAreEqual(){
         int[] sut = {1, 1, 1, 1, 1};
         int[] sortedSut = {1, 1, 1, 1, 1};
         Assertions.assertArrayEquals(sortedSut, Heapsort.heapsort(sut));
     }
 
     @Test
-    public void test3(){
+    public void testIsOne(){
         int[] sut = {-2};
         int[] sortedSut = {-2};
         Assertions.assertArrayEquals(sortedSut, Heapsort.heapsort(sut));
     }
 
     @Test
-    public void test4(){
+    public void testIsEmpty(){
         int[] sut = {};
         int[] sortedSut = {};
         Assertions.assertArrayEquals(sortedSut, Heapsort.heapsort(sut));
     }
 
     @Test
-    public void test5(){
+    public void testIsNull(){
         int[] sut = null;
         int[] sortedSut = null;
         Assertions.assertArrayEquals(sortedSut, Heapsort.heapsort(sut));
     }
 
     @Test
-    public void test6(){
+    public void testRandomWithNegatives(){
         int[] sut = {-3, 5, 0, 19, -9, 1};
         int[] sortedSut = {-9, -3, 0, 1, 5, 19};
         Assertions.assertArrayEquals(sortedSut, Heapsort.heapsort(sut));
     }
 
     @Test
-    public void test7(){
+    public void testIsSorted(){
         int[] sut = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int[] sortedSut = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         Assertions.assertArrayEquals(sortedSut, Heapsort.heapsort(sut));
