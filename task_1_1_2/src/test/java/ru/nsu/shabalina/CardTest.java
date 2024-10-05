bb4fc678b21a850ff000f03607f52a0be22d64be
Shabalina Lilianna <145035472+crying-leika@users.noreply.github.com>
2024-10-06 01:10:10 +0700

    Add files via upload

---
+++ task_1_1_2/src/test/java/ru/nsu/shabalina/CardTest.java
@@ -0,0 +1,59 @@
+package task_1_1_2.src.test.java.ru.nsu.shabalina;
+
+import org.junit.jupiter.api.Test;
+import task_1_1_2.src.main.java.ru.nsu.shabalina.Card;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CardTest {
+
+    @Test
+    void cardTest() {
+        Card.Suit suit;
+        Card.Rank rank;
+
+        Card card;
+
+        for (int i = 0; i < 52; i++) {
+            suit = Card.Suit.values()[i / 13];
+            rank = Card.Rank.values()[i % 13];
+
+            card = new Card(suit, rank);
+
+            assertEquals(suit, card.suit);
+            assertEquals(rank, card.rank);
+        }
+    }
+
+    @Test
+    void pointsTest() {
+        Card card;
+
+        // For each suit
+        for (int i = 0; i < 4; i++) {
+            Card.Suit suit = Card.Suit.values()[i];
+
+            // Ace
+            card = new Card(suit, Card.Rank.Ace);
+            assertEquals(11, card.getValue());
+
+            // 2..10
+            for (int j = 1; j < 10; j++) {
+                card = new Card(suit, Card.Rank.values()[j]);
+                assertEquals(j + 1, card.getValue());
+            }
+
+            // Jack
+            card = new Card(suit, Card.Rank.Jack);
+            assertEquals(10, card.getValue());
+            // Queen
+            card = new Card(suit, Card.Rank.Queen);
+            assertEquals(10, card.getValue());
+            // King
+            card = new Card(suit, Card.Rank.King);
+            assertEquals(10, card.getValue());
+        }
+    }
+}
+
+
---
+++ task_1_1_2/src/test/java/ru/nsu/shabalina/GamerTest.java
@@ -0,0 +1,86 @@
+package task_1_1_2.src.test.java.ru.nsu.shabalina;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import task_1_1_2.src.main.java.ru.nsu.shabalina.Blackjack;
+import task_1_1_2.src.main.java.ru.nsu.shabalina.Gamer;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GamerTest {
+
+    private Gamer gamer;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    public void setUp() {
+        gamer = new Gamer();
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @Test
+    public void testAddCard() {
+        Blackjack.Card card = new Blackjack.Card(Blackjack.Card.Suit.Spades, Blackjack.Card.Rank.Ace);
+        gamer.addCard(card);
+        assertEquals(11, gamer.getScore());
+    }
+
+    @Test
+    public void testGetScore() {
+        gamer.addCard(new Blackjack.Card(Blackjack.Card.Suit.Spades, Blackjack.Card.Rank.Ace));
+        gamer.addCard(new Blackjack.Card(Blackjack.Card.Suit.Hearts, Blackjack.Card.Rank.King));
+        assertEquals(21, gamer.getScore());
+    }
+
+    @Test
+    public void testGetScoreWithAces() {
+        gamer.addCard(new Blackjack.Card(Blackjack.Card.Suit.Spades, Blackjack.Card.Rank.Ace));
+        gamer.addCard(new Blackjack.Card(Blackjack.Card.Suit.Hearts, Blackjack.Card.Rank.Ace));
+        gamer.addCard(new Blackjack.Card(Blackjack.Card.Suit.Diamonds, Blackjack.Card.Rank.Nine));
+        assertEquals(21, gamer.getScore());
+    }
+
+    @Test
+    public void testClearHand() {
+        gamer.addCard(new Blackjack.Card(Blackjack.Card.Suit.Spades, Blackjack.Card.Rank.Ace));
+        gamer.clearHand();
+        assertEquals(0, gamer.getScore());
+    }
+
+    @Test
+    public void testPrintHand() {
+        gamer.addCard(new Blackjack.Card(Blackjack.Card.Suit.Spades, Blackjack.Card.Rank.Ace));
+        gamer.addCard(new Blackjack.Card(Blackjack.Card.Suit.Hearts, Blackjack.Card.Rank.King));
+        gamer.printHand(false, true);
+        String output = outContent.toString();
+        assertTrue(output.contains("Ваши карты: [Туз Пики, Король Червы] => 21"));
+    }
+
+    @Test
+    public void testPrintHandClosed() {
+        gamer.addCard(new Blackjack.Card(Blackjack.Card.Suit.Spades, Blackjack.Card.Rank.Ace));
+        gamer.addCard(new Blackjack.Card(Blackjack.Card.Suit.Hearts, Blackjack.Card.Rank.King));
+        gamer.printHand(true, false);
+        String output = outContent.toString();
+        assertTrue(output.contains("Карты дилера: [Туз Пики, <закрытая карта>]"));
+    }
+
+    @Test
+    public void testPrintHandEmpty() {
+        gamer.printHand(false, true);
+        String output = outContent.toString();
+        assertTrue(output.contains("Нет карт на руках"));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(originalOut);
+    }
+}
+
+
