f69b25e8495b428941898a597bc4f8ecebc01670
leika <liliannashabalina124@gmail.com>
2024-10-06 14:16:05 +0700

    import fix

--- task_1_1_2/src/test/java/ru/nsu/shabalina/GamerTest.java
+++ task_1_1_2/src/test/java/ru/nsu/shabalina/GamerTest.java
@@ -3,7 +3,6 @@ package ru.nsu.shabalina;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
