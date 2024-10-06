e1d9709ec136e58d3114d7d333baa9a232df3966
leika <liliannashabalina124@gmail.com>
2024-10-06 14:33:20 +0700

    import groop

--- task_1_1_2/src/test/java/ru/nsu/shabalina/BlackjackTest.java
+++ task_1_1_2/src/test/java/ru/nsu/shabalina/BlackjackTest.java
@@ -1,15 +1,16 @@
 package ru.nsu.shabalina;
 
-import org.junit.jupiter.api.Test;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.List;
 import java.util.Scanner;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
