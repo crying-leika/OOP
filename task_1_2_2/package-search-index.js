6afda5a2a3f7a01a49825934b50119364501b6ca
crying-leika <crying-leika@users.noreply.github.com>
2024-12-11 01:38:27 +0000

    Deploying to gh-pages from @ crying-leika/OOP@7bedd0be8d748d95007d99cda699a548f11185ec 🚀

--- task_1_2_2/package-search-index.js
+++ task_1_2_2/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.shabalina"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.shabalina"}];updateSearchResults();
