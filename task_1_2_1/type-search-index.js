73bf9a095bbb50ceb745992fe68ecf9a65185a8c
crying-leika <crying-leika@users.noreply.github.com>
2024-12-14 10:43:41 +0000

    Deploying to gh-pages from @ crying-leika/OOP@af66a778991a38e2cc6b594a42a5c1f0641ea4e1 🚀

---
+++ task_1_2_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.shabalina","l":"Element"},{"p":"ru.nsu.shabalina","l":"HashTable"}];updateSearchResults();
